6c76cde98db968c39205572110597c8048cef332
Anand Kumar <anandjee187@gmail.com>
2023-06-05 12:08:07 +0530

    email itegration

---
+++ server/routes/router.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = new express.Router();
+const nodemailer = require("nodemailer");
+const formController = require('../controllers/formController');
+
+
+
+
+router.get("/health", (req, res) => {
+    res.send({
+        data: "I am healthy"
+    })
+});
+
+
+//save formData
+router.post('/submit', formController.submitForm);
+
+
+// send mail
+router.post("/register", (req, res) => {
+
+
+    const { email, username } = req.body;
+
+
+    try {
+
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
+            }
+        });
+
+
+        const mailOptions = {
+            from: `"Bridge Health" <${process.env.EMAIL}>`,
+            to: `${username}   <${email}> `,
+            subject: "Welcome to Bridge Health",
+            html: ` 
+            <p>Dear ${username},</p>
+            <p>Thank you for registering on <span> <strong>Bridge Health.<strong> <span></p>
+            <p>We appreciate your interest and will review your information promptly.</p>
+            <p>Our health experts will contact you soon.</p>
+            <p>Best regards,<br> <strong>Bridge Health </strong> </p>`
+        };
+
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log("Error" + error)
+            } else {
+                console.log("Email sent:" + info.response);
+                res.status(201).json({ status: 201, info })
+            }
+        })
+
+
+    } catch (error) {
+        console.log("Error" + error);
+        res.status(401).json({ status: 401, error })
+    }
+});
+
+
+
+
+module.exports = router;
