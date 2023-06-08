60f28e9af57a37c0dfaa64c016166d3406a4b333
Anand Kumar <anandjee187@gmail.com>
2023-06-08 16:59:56 +0530

    Revert "merge"
    This reverts commit 4f37011d37606b0feb28166d5680c75281e2a173.

--- server/app.jsx
+++ server/app.jsx
@@ -1,5 +1,4 @@
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
-console.log("path ",`.env.${process.env.NODE_ENV}`)
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./routes/router");
