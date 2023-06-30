e0adf4ab143e5f44184d1d54df448582db6c285f
techonline-dev <shubham9771394656@gmail.com>
2023-06-30 08:47:42 +0530

    updated

--- src/markup/pages/experience-centre.jsx
+++ src/markup/pages/experience-centre.jsx
@@ -10,7 +10,7 @@ class experienceCentre extends Component {
             <>
                 <Header />
                 {/* <!-- Page Banner Section --> */}
-                <section class="page-banner">
+                <section class="page-banner commimgsoon">
                     <div className="page-banner-bg" style={{ backgroundImage: "url(" + aboutbg + ")" }}></div>
                     <div class="bottom-rotten-curve alternate"></div>
                 </section>
--- src/markup/pages/media-and-pr.jsx
+++ src/markup/pages/media-and-pr.jsx
@@ -14,7 +14,7 @@ class mediaAndPR extends Component {
             <>
                 <Header />
                 {/* <!-- Page Banner Section --> */}
-                <section class="page-banner">
+                <section class="page-banner commimgsoon">
                     <div className="page-banner-bg" style={{ backgroundImage: "url(" + aboutbg + ")" }}></div>
                     <div class="bottom-rotten-curve alternate"></div>
                 </section>
